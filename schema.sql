8e6c205b05afda52b4a229bf9b21734b7613b5f9
WD40 <69653174+duncangw1@users.noreply.github.com>
2020-11-23 11:18:24 -0500

    Created schema and ran in MySql to create the DB. Also created empty seeds.sql file.

---
+++ schema.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS empTrack_db;
+CREATE DATABASE empTrack_db;
+
+USE empTrack_db;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
